cc4db5de495d24155d79a0e4faf3501878d0209e
Regis Cuba <120754196+devregiscuba@users.noreply.github.com>
2026-04-06 19:12:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -134,6 +134,7 @@ Extraia TODO texto e TODOS números, considerando que na imagem pode conter codi
                                 - NÃO inclua texto explicativo
                                 - se não houver quantity, usar 1
                                 - Ignore qualquer texto que não seja produto
+                                - Se a barra(/) for pequena pode ser que seja o numero 1
                                 - Partnumber nao tem ponto(.) ou virgua(,)
                                 - Geralmente a quantity na imagem, fica isolada antes ou depois do Partnumber 
                                  - Ignore qualquer palavra, exemplo: "1 - sangria - 74RD7899E" deve ignorar 'sangria' pq é uma palavra, ou seja, se houver 3 letras consecutivas pode ignorar a palavra ficando quantidade 1 e partnumber 74RD7899E.
